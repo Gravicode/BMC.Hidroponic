e5f61289bbf5ed4e034db2c833c7c82eafc0b7b0
Mif Masterz <mifmasterz@yahoo.com>
2021-10-05 16:44:05 +0700

    update ref

--- BMC.Hidroponic/xx/BMC.Hidroponic.Gateway/Program.cs
+++ BMC.Hidroponic/xx/BMC.Hidroponic.Gateway/Program.cs
@@ -46,7 +46,7 @@ namespace BMC.Hidroponic.Gateway
             string ClientUser = ConfigurationManager.AppSettings["MqttUser"];
             string ClientPass = ConfigurationManager.AppSettings["MqttPass"];
 
-            MqttClient = new MqttClient(IPAddress.Parse(IPBrokerAddress));
+            MqttClient = new MqttClient(IPBrokerAddress);
 
             // register a callback-function (we have to implement, see below) which is called by the library when a message was received
             MqttClient.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
